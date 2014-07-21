f6b50cbe2724f7e8116aed24322ed36a8476c02c
Philippe Coval <philippe.coval@open.eurogiciel.org>
2014-07-21 11:39:19 +0200

    debian: ubuntu: import: https://launchpad.net/ubuntu/+archive/primary/+files/weston_1.5.0-2ubuntu1.dsc
    Change-Id: Ice5f89828144ee8558080852d036ab95d6215920
    Signed-off-by: Philippe Coval <philippe.coval@open.eurogiciel.org>

--- autogen.sh
+++ autogen.sh
@@ -7,3 +7,12 @@ test -n "$srcdir" || srcdir=.
   autoreconf --force -v --install
 ) || exit
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+#! /bin/sh
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+(
+  cd "$srcdir" &&
+  autoreconf --force -v --install
+) || exit
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
